34f233ee26cd08598db3a63f41bd4765c706fe79
宮前　賢太　◇ <kenta_miyamae@dwango.co.jp>
2026-07-30 16:07:35 +0900

    Clarify Bayesian and information theory formulas

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function saveAnswer(q, isCorrect) {
 
 if (quizId) {
     const script = document.createElement('script');
-    script.src = `js/data/${quizId}.js?v=20260730-mathcalc`;
+    script.src = `js/data/${quizId}.js?v=20260730-formulaguide`;
     script.onload = () => window.quizData ? (currentQuizData = window.quizData, initApp()) : showError('問題データの形式が正しくありません。');
     script.onerror = () => showError('指定された問題データが見つかりません。');
     document.body.appendChild(script);
