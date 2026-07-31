57df1f9d979128cf840cbdd341fa685702811705
kohey7hara <166122282+kohey7hara@users.noreply.github.com>
2026-07-31 17:31:08 +0900

    Add distance calculation exercises to ML basics

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function saveAnswer(q, isCorrect) {
 
 if (quizId) {
     const script = document.createElement('script');
-    script.src = `js/data/${quizId}.js?v=20260731-info-calculations`;
+    script.src = `js/data/${quizId}.js?v=20260731-distance-calculations`;
     script.onload = () => window.quizData ? (currentQuizData = window.quizData, initApp()) : showError('問題データの形式が正しくありません。');
     script.onerror = () => showError('指定された問題データが見つかりません。');
     document.body.appendChild(script);
