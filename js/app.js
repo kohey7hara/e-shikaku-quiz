32791e0438e2dde66a5ef7d6cee6c2eb8a8dbcc1
宮前　賢太　◇ <kenta_miyamae@dwango.co.jp>
2026-08-19 14:55:09 +0900

    Remove obsolete bootcamp session

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ function saveAnswer(q, isCorrect) {
 
 if (quizId) {
     const script = document.createElement('script');
-    script.src = `js/data/${quizId}.js?v=20260819-distance-diagrams-v1`;
+    script.src = `js/data/${quizId}.js?v=20260819-remove-bootcamp-v1`;
     script.onload = () => window.quizData ? (currentQuizData = window.quizData, initApp()) : showError('問題データの形式が正しくありません。');
     script.onerror = () => showError('指定された問題データが見つかりません。');
     document.body.appendChild(script);
